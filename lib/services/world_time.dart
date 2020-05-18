a3732bdcb6b12b5b1488991bdc556628b9b8289c
masterofboards <63799818+masterofboards@users.noreply.github.com>
2020-05-18 06:44:19 -0700

    Initial commit

---
+++ lib/services/world_time.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location;
+  String flag;
+  String url;
+  String time;
+  bool isDaytime;
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    Response response = await get("http://worldtimeapi.org/api/timezone/$url");
+    Map data = jsonDecode(response.body);
+    String dateTime = data["datetime"];
+    String offset = data["utc_offset"].substring(1, 3);
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    isDaytime = now.hour > 6 && now.hour < 19;
+    time = DateFormat.jm().format(now);
+  }
+}
